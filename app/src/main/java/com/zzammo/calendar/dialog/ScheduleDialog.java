4909bf4192170a92b702a2b6de32a790b46add0c
DongGi-create <84391428+DongGi-create@users.noreply.github.com>
2022-12-30 18:28:48 +0900

    다이얼로그 수정

--- app/src/main/java/com/zzammo/calendar/dialog/ScheduleDialog.java
+++ app/src/main/java/com/zzammo/calendar/dialog/ScheduleDialog.java
@@ -4,9 +4,12 @@ import android.app.Dialog;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import com.zzammo.calendar.R;
@@ -25,6 +28,11 @@ public class ScheduleDialog extends Dialog {
     EditText location_et;
     TimePicker timePicker;
     Button save_btn;
+    TextView depart_date;
+    TextView depart_clock;
+    TextView arrive_date;
+    TextView arrive_clock;
+    String visiblemode;
 
     public ScheduleDialog(Context context, Long dateTime) {
         super(context, android.R.style.Theme_Translucent_NoTitleBar);
@@ -42,13 +50,56 @@ public class ScheduleDialog extends Dialog {
         getWindow().setAttributes(lpWindow);
 
         setContentView(R.layout.schedule_dialog);
-
         dialog = this;
+
+        depart_date = findViewById(R.id.depart_date);
+        depart_clock = findViewById(R.id.depart_clock);
+        arrive_date = findViewById(R.id.arrive_date);
+        arrive_clock = findViewById(R.id.arrive_clock);
 
         title_et = this.findViewById(R.id.schedule_dialog_title);
         location_et = this.findViewById(R.id.schedule_dialog_location);
         timePicker = this.findViewById(R.id.schedule_dialog_timePicker);
         save_btn = this.findViewById(R.id.schedule_dialog_saveBtn);
+
+        //타임피커 보이게
+        depart_clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("minseok","departtime!");
+                if(timePicker.getVisibility()==View.GONE){
+                    timePicker.setVisibility(View.VISIBLE);
+                    visiblemode = "departtime";
+                    Log.d("minseok","departtime");
+                }
+                else if(timePicker.getVisibility()==View.VISIBLE){
+                    timePicker.setVisibility(View.GONE);
+                    visiblemode = "nomode";
+                    Log.d("minseok","nomode");
+                }
+            }
+        });
+        arrive_clock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(timePicker.getVisibility()==View.GONE){
+                    timePicker.setVisibility(View.VISIBLE);
+                    visiblemode = "arrivetime";
+                }
+                if(timePicker.getVisibility()==View.VISIBLE){
+                    timePicker.setVisibility(View.GONE);
+                    visiblemode = "nomode";
+                }
+            }
+        });
+
+        timePicker.setOnTimeChangedListener(new TimePicker.OnTimeChangedListener() {
+            @Override
+            public void onTimeChanged(TimePicker timePicker, int hour, int minute) {
+                if(visiblemode.equals("departtime")) puttime(depart_clock,hour,minute);
+                else if(visiblemode.equals("departtime"))puttime(depart_clock,hour,minute);
+            }
+        });
 
         save_btn.setOnClickListener(view -> {
             String title = title_et.getText().toString();
@@ -73,5 +124,13 @@ public class ScheduleDialog extends Dialog {
 
             dialog.dismiss();
         });
+    }
+    private void puttime(TextView a, int hour, int minute){
+        if (hour > 12) {
+            hour -= 12;
+            a.setText("오후 " + hour + "시 " + minute + "분 선택");
+        } else {
+            a.setText("오전 " + hour + "시 " + minute + "분 선택");
+        }
     }
 }
