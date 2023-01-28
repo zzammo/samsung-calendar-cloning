ec07f7fdf99a28f31cbe5b9d41a434e7dc95a421
이동원 <retomoo@naver.com>
2023-01-28 20:57:16 +0900

    기온 날씨 음력날짜 ui적용

--- app/src/main/java/com/zzammo/calendar/activity/schedule_main.java
+++ app/src/main/java/com/zzammo/calendar/activity/schedule_main.java
@@ -1,33 +1,62 @@
 package com.zzammo.calendar.activity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.zzammo.calendar.R;
+import com.zzammo.calendar.lunar.LunarCalendar;
+import com.zzammo.calendar.weather.WeatherApiExplorer;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 
 public class schedule_main extends AppCompatActivity {
 
     ImageView add_schedule;
+    Context context;
+    HashMap<LocalDate, HashMap<String, Integer>> weather;
+    TextView temperature, lunardate, or;
+    ImageView weatherView;
+    MaterialCalendarView calendarView;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.schedule_main);
+        context = this;
+        weather = new HashMap<>();
+
+        calendarView = findViewById(R.id.calendarView);
 
         getSupportActionBar().setElevation(0); // appbar shadow remove
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 툴바 홈버튼 활성화
         // ↓툴바의 홈버튼의 이미지를 변경(기본 이미지는 뒤로가기 화살표)
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_density_medium_24);
 
+        temperature = findViewById(R.id.temperature);
+        lunardate = findViewById(R.id.lunardate);
+        or = findViewById(R.id.or);
+        weatherView = findViewById(R.id.weather);
         add_schedule=findViewById(R.id.add_schedule);
 
         add_schedule.setOnClickListener(new View.OnClickListener() {
@@ -37,6 +66,10 @@ public class schedule_main extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        LocalDate localDate = LocalDate.parse(LunarCalendar.Solar2Lunar(LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"))), DateTimeFormatter.ofPattern("yyyyMMdd"));
+        lunardate.setText("음력 " + localDate.getMonthValue() + "월 " + localDate.getDayOfMonth() + "일");
+        getWeather(weather, 35.887390,128.611629);
 
     }
 
@@ -50,6 +83,7 @@ public class schedule_main extends AppCompatActivity {
         return true;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public boolean onOptionsItemSelected (MenuItem item)
     {
@@ -59,6 +93,7 @@ public class schedule_main extends AppCompatActivity {
         {
             case R.id.today_move:
                 toast.setText("Select today_move");
+                calendarView.setSelectedDate(CalendarDay.today());
                 break;
             case R.id.name_search:
                 toast.setText("Select name_search");
@@ -68,5 +103,52 @@ public class schedule_main extends AppCompatActivity {
         toast.show();
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void getWeather(HashMap<LocalDate, HashMap<String, Integer>> weather, double latitude, double longitude){
+        WeatherApiExplorer weatherApiExplorer = new WeatherApiExplorer(context);
+        new Thread(new Runnable() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void run() {
+                try {
+                    weatherApiExplorer.getWeather(weather, latitude, longitude);
+                    Log.d("WeGlonD", weather.toString());
+                    temperature.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            or.setVisibility(View.VISIBLE); temperature.setVisibility(View.VISIBLE); weatherView.setVisibility(View.VISIBLE);
+                            temperature.setText(weather.get(LocalDate.now()).get("TMP").toString() + "ºC");
+                            int pty = weather.get(LocalDate.now()).get("PTY");
+                            int sky = weather.get(LocalDate.now()).get("SKY");
+                            if(pty > 0){
+                                if(pty == 1 || pty == 4){
+                                    weatherView.setImageResource(R.drawable.rainy_2_svgrepo_com);
+                                }
+                                else if(pty == 2){
+                                    weatherView.setImageResource(R.drawable.cloud_snow_rain_svgrepo_com);
+                                }
+                                else{
+                                    weatherView.setImageResource(R.drawable.snow_outline_svgrepo_com);
+                                }
+                            }
+                            else{
+                                if(sky == 1){
+                                    weatherView.setImageResource(R.drawable.ic_baseline_wb_sunny_24);
+                                }
+                                else if(sky == 2){
+                                    weatherView.setImageResource(R.drawable.cloudy_svgrepo_com);
+                                }
+                                else{
+                                    weatherView.setImageResource(R.drawable.ic_twotone_wb_cloudy_24);
+                                }
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 }
--- app/src/main/java/com/zzammo/calendar/weather/WeatherApiExplorer.java
+++ app/src/main/java/com/zzammo/calendar/weather/WeatherApiExplorer.java
@@ -1,5 +1,12 @@
 package com.zzammo.calendar.weather;
 
+import static java.lang.Math.asin;
+import static java.lang.Math.cos;
+import static java.lang.Math.log;
+import static java.lang.Math.pow;
+import static java.lang.Math.sin;
+import static java.lang.Math.tan;
+
 import android.content.Context;
 import android.os.Build;
 import android.util.Log;
@@ -31,9 +38,11 @@ public class WeatherApiExplorer {
     Context context;
     private final String key = "Iw10j3Acrg3WrDmYl0%2FXMM08AJStZ0qjmSPPaS2Lb7oazhsvcADHN2v%2BkkSotAJWWHqErRHUUK9no0Z8o6hLrA%3D%3D";
     private LocalTime[] times = new LocalTime[8];
+    public final lamc_parameter map = new lamc_parameter();
     public WeatherApiExplorer(Context context){ this.context = context; }
+    int nx, ny;
     @RequiresApi(api = Build.VERSION_CODES.O)
-    public void getWeather() throws IOException {
+    public void getWeather(HashMap<LocalDate, HashMap<String, Integer>> weather, double lat, double lng) throws IOException {
         // 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300
         for(int i = 0; i < 8; i++) {
             times[i] = LocalTime.of(2 + 3 * i, 10);
@@ -45,22 +54,26 @@ public class WeatherApiExplorer {
             now = LocalTime.of(23,30);
         }
         String date = ""; String time = "";
-        for(int i = 6; i > -1; i--){
+        int i;
+        for(i = 6; i > -1; i--){
             if(now.isAfter(times[i])){
                 date = today.format(DateTimeFormatter.ofPattern("yyyyMMdd"));
                 time = times[i].minusMinutes(10).format(DateTimeFormatter.ofPattern("HHmm"));
                 break;
             }
         }
+        lamcproj(lat, lng, map);
+        String xx = Integer.toString(nx); String yy = Integer.toString(ny);
+        Log.d("WeGlonD", "Position - lat : " + lat + " lng : " + lng + " -> nx : " + xx + " ny : " + yy);
         StringBuilder urlBuilder = new StringBuilder("http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getVilageFcst"); /*URL*/
         urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=" + key); /*Service Key*/
         urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지번호*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1000", "UTF-8")); /*한 페이지 결과 수*/
+        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("1500", "UTF-8")); /*한 페이지 결과 수*/
         urlBuilder.append("&" + URLEncoder.encode("dataType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*요청자료형식(XML/JSON) Default: XML*/
         urlBuilder.append("&" + URLEncoder.encode("base_date","UTF-8") + "=" + URLEncoder.encode(date, "UTF-8")); /*‘21년 6월 28일 발표*/
         urlBuilder.append("&" + URLEncoder.encode("base_time","UTF-8") + "=" + URLEncoder.encode(time, "UTF-8")); /*05시 발표(정시단위) */
-        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode("55", "UTF-8")); /*예보지점의 X 좌표값*/
-        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode("127", "UTF-8")); /*예보지점의 Y 좌표값*/
+        urlBuilder.append("&" + URLEncoder.encode("nx","UTF-8") + "=" + URLEncoder.encode(xx, "UTF-8")); /*예보지점의 X 좌표값*/
+        urlBuilder.append("&" + URLEncoder.encode("ny","UTF-8") + "=" + URLEncoder.encode(yy, "UTF-8")); /*예보지점의 Y 좌표값*/
         URL url = new URL(urlBuilder.toString());
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
         conn.setRequestMethod("GET");
@@ -73,8 +86,8 @@ public class WeatherApiExplorer {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
 
-        HashMap<LocalDate, HashMap<String, Integer>> weather = new HashMap<>();
         JSONParser parser = new JSONParser();
+        String curr = times[i].plusMinutes(50).format(DateTimeFormatter.ofPattern("HHmm"));
         try {
             JSONObject jsonObject = (JSONObject) parser.parse(rd);
             //Log.d("WeGlonD", jsonObject.toJSONString());
@@ -92,7 +105,8 @@ public class WeatherApiExplorer {
                     LocalDate weatherdate = LocalDate.parse(json.get("fcstDate").toString(), DateTimeFormatter.ofPattern("yyyyMMdd"));
                     String type = (String) json.get("category");
 
-                    if(((String)json.get("fcstTime")).equals("0000")) continue;
+                    String yebotime = (String)json.get("fcstTime");
+                    if(yebotime.equals("0000")) continue;
 
                     if(!weather.containsKey(weatherdate)){
                         weather.put(weatherdate, new HashMap<String, Integer>());
@@ -101,43 +115,51 @@ public class WeatherApiExplorer {
 
                     HashMap<String, Integer> data = weather.get(weatherdate);
                     if(type.equals("SKY")){
-                        sky.get(weatherdate).add(Integer.parseInt((String)json.get("fcstValue")));
+                        if(weatherdate.equals(today)){
+                            if(yebotime.equals(curr)){
+                                data.put("SKY", Integer.parseInt((String) json.get("fcstValue")));
+                            }
+                        }
+                        else {
+                            sky.get(weatherdate).add(Integer.parseInt((String) json.get("fcstValue")));
+                        }
                     }
                     else if(type.equals("PTY")){
-                        if(!data.containsKey("PTY")){
-                            data.put("PTY", Integer.parseInt((String)json.get("fcstValue")));
+                        if(weatherdate.equals(today)){
+                            if(yebotime.equals(curr)){
+                                data.put("PTY", Integer.parseInt((String) json.get("fcstValue")));
+                            }
                         }
                         else{
-                            data.replace("PTY", Integer.max(data.get("PTY"), Integer.parseInt((String)json.get("fcstValue"))));
+                            if(!data.containsKey("PTY")){
+                                data.put("PTY", Integer.parseInt((String)json.get("fcstValue")));
+                            }
+                            else{
+                                data.replace("PTY", Integer.max(data.get("PTY"), Integer.parseInt((String)json.get("fcstValue"))));
+                            }
                         }
                     }
-                    else if(type.equals("TMP") && weatherdate.equals(today)){
+                    else if(type.equals("TMP") && weatherdate.equals(today) && yebotime.equals(curr)){
                         int temperature = Integer.parseInt((String)json.get("fcstValue"));
-                        if(temperature > todaymax) todaymax = temperature;
-                        if(temperature < todaymin) todaymin = temperature;
+                        data.put("TMP", temperature);
                     }
                     else if(type.equals("TMX")){
-                        if(weatherdate.equals(today)){
-                            todaymax = (int)Double.parseDouble((String)json.get("fcstValue"));
-                        }
-                        else{
+                        if(!weatherdate.equals(today)){
                             data.put("MAX", (int)Double.parseDouble((String)json.get("fcstValue")));
                         }
                     }
                     else if(type.equals("TMN")){
-                        if(weatherdate.equals(today)){
-                            todaymin = (int)Double.parseDouble((String)json.get("fcstValue"));
-                        }
-                        else{
+                        if(!weatherdate.equals(today)){
                             data.put("MIN", (int)Double.parseDouble((String)json.get("fcstValue")));
                         }
                     }
                 }
             }
 
-            weather.get(today).put("MAX", todaymax);
-            weather.get(today).put("MIN", todaymin);
+//            weather.get(today).put("MAX", todaymax);
+//            weather.get(today).put("MIN", todaymin);
             for (LocalDate key : sky.keySet()) {
+                if(key.equals(today)) continue;
                 ArrayList<Integer> skies = sky.get(key);
                 int sum = 0, size = skies.size();
                 for(Integer s : skies){
@@ -162,5 +184,63 @@ public class WeatherApiExplorer {
 //            e.printStackTrace();
 //            Log.d("WeGlonD", "Weather get item Failed");
 //        }
+    }
+
+    public class lamc_parameter{
+        double Re; /* 사용할 지구반경 [ km ] */
+        double grid; /* 격자간격 [ km ] */
+        double slat1; /* 표준위도 [degree] */
+        double slat2; /* 표준위도 [degree] */
+        double olon; /* 기준점의 경도 [degree] */
+        double olat; /* 기준점의 위도 [degree] */
+        double xo; /* 기준점의 X좌표 [격자거리] */
+        double yo; /* 기준점의 Y좌표 [격자거리] */
+        int first; /* 시작여부 (0 = 시작) */
+
+        public lamc_parameter(){
+            Re = 6371.00877; // 지도반경
+            grid = 5.0; // 격자간격 (km)
+            slat1 = 30.0; // 표준위도 1
+            slat2 = 60.0; // 표준위도 2
+            olon = 126.0; // 기준점 경도
+            olat = 38.0; // 기준점 위도
+            xo = 210/grid; // 기준점 X좌표
+            yo = 675/grid; // 기준점 Y좌표
+            first = 0;
+        }
+    }
+
+    void lamcproj(double lat, double lng, lamc_parameter map){
+        double PI, DEGRAD, RADDEG;
+        double re, olon, olat, sn, sf, ro;
+        double slat1, slat2, alon, alat, xn, yn, ra, theta;
+
+        PI = asin(1.0)*2.0;
+        DEGRAD = PI/180.0;
+        RADDEG = 180.0/PI;
+
+        re = map.Re/map.grid;
+        slat1 = map.slat1 * DEGRAD;
+        slat2 = map.slat2 * DEGRAD;
+        olon = map.olon * DEGRAD;
+        olat = map.olat * DEGRAD;
+
+        sn = tan(PI*0.25 + slat2*0.5)/tan(PI*0.25 + slat1*0.5);
+        sn = log(cos(slat1)/cos(slat2))/log(sn);
+        sf = tan(PI*0.25 + slat1*0.5);
+        sf = pow(sf,sn)*cos(slat1)/sn;
+        ro = tan(PI*0.25 + olat*0.5);
+        ro = re*sf/pow(ro,sn);
+        map.first = 1;
+
+        ra = tan(PI*0.25+lat*DEGRAD*0.5);
+        ra = re*sf/pow(ra,sn);
+        theta = lng*DEGRAD - olon;
+        if (theta > PI) theta -= 2.0*PI;
+        if (theta < -PI) theta += 2.0*PI;
+        theta *= sn;
+		nx = (int)((float)(ra*sin(theta)) + map.xo + 1.5);
+		ny = (int)((float)(ro - ra*cos(theta)) + map.yo + 1.5);
+
     }
 }
